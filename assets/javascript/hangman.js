3d5cb9a319ee5a76fd6add8cb5156e08bd84c11b
Nate Miller <nathanmiller9@yahoo.com>
2017-02-18 08:23:19 -0800

    added image files and significant edits to html javascript and css files

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,15 +1,11 @@
-var wordList = [
+var words = [
 	"cincinnati",
 	"dallas",
 	"cleveland",
-	"new york",
 	"pittsburgh",
 	"sacramento",
-	"san francisco",
-	"los angeles",
 	"omaha",
 	"minneapolis",
-	"oklahoma city",
 	"baltimore",
 	"richmond",
 	"atlanta",
@@ -18,9 +14,6 @@ var wordList = [
 	"boston",
 	"seattle",
 	"portland",
-	"san diego",
-	"las vegas",
-	"salt lake city",
 	"provo",
 	"helena",
 	"boise",
@@ -36,9 +29,6 @@ var wordList = [
 	"tallahassee",
 	"birmingham",
 	"jackson",
-	"jefferson city",
-	"sioux city",
-	"des moines",
 	"pierre",
 	"bismarck",
 	"charleston",
@@ -48,10 +38,8 @@ var wordList = [
 	"manchester",
 	"raleigh",
 	"charlotte",
-	"san jose",
 	"madison",
 	"milwaukee",
-	"little rock",
 	"annapolis",
 	"charlotte",
 	"bend",
@@ -64,34 +52,22 @@ var wordList = [
 	"anchorage",
 	"providence",
 	"denver",
-	"santa fe",
 	"albuquerque",
 	"phoenix",
-	"tempe",
-	"tampa bay",
-	"san antonio",
-	"santa barbara", 
+	"tempe", 
 	"lansing",
 	"cheyenne",
-	"colorado springs",
 	"boulder",
 	"fargo",
 	"rochester",
 	"harrisburg", 
 	"columbus",
 	"columbia",
-	"fort lauderdale",
 	"biloxi",
-	"baton rouge",
 	"shreveport",
 	"tulsa",
-	"kansas city",
-	"saint louis",
 	"peoria",
-	"south bend",
 	"durango",
-	"carson city",
-	"saint paul",
 	"montgomery",
 	"frankfort",
 	"knoxville",
@@ -100,8 +76,6 @@ var wordList = [
 	"springfield",
 	"wheeling",
 	"chattanooga",
-	"el paso",
-	"new orleans",
 	"buffalo",
 	"billings",
 	"anaheim",
@@ -109,7 +83,6 @@ var wordList = [
 	"trenton",
 	"scranton",
 	"savannah",
-	"palm springs",
 	"spokane",
 	"mobile",
 	"flint",
@@ -125,14 +98,9 @@ var wordList = [
 	"asheville",
 	"dover",
 	"wilmington",
-	"corpus christi",
 	"jacksonville",
-	"fort worth",
-	"washington dc",
 	"louisville",
 	"tucson",
-	"long beach",
-	"virginia beach",
 	"oakland",
 	"wichita",
 	"bakersfield",
@@ -141,119 +109,90 @@ var wordList = [
 	"toledo",
 	"greensboro",
 	"plano",
-	"fort wayne",
 	"orlando",
 	"durham",
 	"lubbock",
 	"irvine",
-	"san bernardino"
 ];
 
-var chosenWord = ""; //stores the random word chosen from wordList using math.random
-var letterInChosenWord = []; //will break up chosen word into independent letters so that the user can guess it and so that we can manipulate it through the logic of the game.. using a for loop
-var numBlanks = 0; //will hold the number of letters in the chosenWord
-var blanksAndSuccesses = []; //will hold whether the user guessed the letter right or wrong
-var wrongGuesses = []; //will hold wrongly guessed letters
+var wordChoice = "";
+var wordString = [];
+var wordLength = 0;
+var letterChoice = [];
+var wrongChoices = [];
+var wins = 0;
+var losses = 0;
+var guesses = 9;
 
-var winCounter = 0; //will increase by one if user wins
-var lossCounter = 0; //will increase by one if user loses
-var numGuesses = 9; //will be set to nine, amount of chances you have before the game is over
+function startOfGame (){
 
-function startGame(){
-	/*
-1.  select a word at random
-2.  want to break up that random word into letters and replace them with underscores
-			for loop
-3.  want to add those underscores to the HTML
-4.  at start of the game, numGuesses always equals 9, blankAndSuccesses is an empty array, and wrongGuesses is also 0
+	wrongChoices = [];
+	letterChoice = [];
+	guesses = 9;
+	
+	wordChoice = words[Math.floor(Math.random() * words.length)];
+	wordString = wordChoice.split("");
+	wordLength = wordString.length;
+	console.log(wordChoice);
+	console.log(wordLength);
 
-	*/
-wrongGuesses = [];
-numGuesses = 9;
-blanksAndSuccesses = [];
-
-
-chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
-lettersInChosenWord = chosenWord.split("");
-numBlanks = lettersInChosenWord.length;
-console.log(chosenWord);
-console.log(numBlanks);
-
-for(var i = 0; i < numBlanks; i++){
-	blanksAndSuccesses.push("_");
-}
-console.log(blanksAndSuccesses);
-document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
-document.getElementById('guesses-left').innerHTML = numGuesses;
-}
-
-function checkLetters(letter){
-	/*
-	1.  Compare to see if the letter the user picks matches any of the leters in the word
-	2.  Want a conditional statement to determine if the letter the user picked is in the word.  If so, do something, if not, do something else.
-	3.  If the user is wrong we want to decrease the numGuesses variables by one
-*/
-
-var letterInWord = false;
-
-for(var i = 0; i < numBlanks; i++){
-	if(chosenWord[i] === letter){
-		letterInWord = true;
-
-}
-}
-    if(letterInWord){
-    	for(i = 0; i < numBlanks; i++){
-    		if(chosenWord[i] === letter){
-		blanksAndSuccesses[i] = letter;
-    	}
-    	
-    	}
-    }
-
-    else{
-    	numGuesses --;
-    	wrongGuesses.push(letter)
-    }
-    
-}
-
-function roundComplete(){
-	/*
-	1. Its going to update the HTML with letters that are in the word
-	2. Its going to update the HTML with guesses we have left
-	3. Its going to update the HTML to show the wrong guesses
-	4. Its going to deteremine whether the user won the game or not
-	*/
-	document.getElementById('word-blank').innerHTML = blanksAndSuccesses.join(" ");
-	document.getElementById('guesses-left').innerHTML = numGuesses;
-	document.getElementById('wrong-guesses').innerHTML = wrongGuesses.join(" ");
-
-	console.log(lettersInChosenWord);
-	console.log(blanksAndSuccesses);
-	if(lettersInChosenWord.join(" ") === blanksAndSuccesses.join(" ")){
-		winCounter++;
-		alert("You win!!");
-		document.getElementById('win-counter').innerHTML = winCounter;
-		startGame();
+	for(var i = 0; i < wordLength; i++){
+			letterChoice.push("_");
 	}
-	else if (numGuesses === 0){
-		document.getElementById('loss-counter').innerHTML = lossCounter++;
+	console.log(letterChoice);
+	document.getElementById('clue').innerHTML = letterChoice.join(" ");
+	document.getElementById('guesses-left').innerHTML = guesses;
+	// document.getElementById('result').innerHTML =  "";
+}
+
+function guessCheck(guess){
+	
+	var letterGuess = false;
+
+	for(var i = 0; i < wordLength; i++){
+		if(wordChoice[i] === guess){
+			letterGuess = true;
+		}
+	}
+	if(letterGuess){
+		for(i=0; i < wordLength; i++){
+			if(wordChoice[i] === guess){
+				letterChoice[i] = guess;
+			}
+		}
+	}
+	else{
+		guesses--;
+		wrongChoices.push(guess);
+	}
+}
+
+function game(){
+	document.getElementById('clue').innerHTML = letterChoice.join(" ");
+	document.getElementById('guesses-left').innerHTML = guesses;
+	document.getElementById('wrong-guesses').innerHTML = wrongChoices.join(" ");
+
+	console.log(wordString);
+	console.log(letterChoice);
+	if(wordString.join(" ") === letterChoice.join(" ")){
+		wins++;
+		alert(wordChoice.toUpperCase() + " is correct! You win!!");
+		// document.getElementById('result').innerHTML = wordChoice.toUpperCase() + " is correct! You win!!";
+		document.getElementById('wins').innerHTML = wins;
+		startOfGame();
+	}
+	else if (guesses === 0){
+		document.getElementById('losses').innerHTML = losses+=1;
 		document.getElementById('wrong-guesses').innerHTML = "";
-		alert("you don't have any more guesses");
-		startGame();
+		alert("The city was " + wordChoice + ".  Better luck next time!");
+		startOfGame();
 	}
-
 }
 
-startGame();
+startOfGame();
 document.onkeyup = function(event){
-	/*
-	1. Its going to take in the letter that we type in
-	2. Its going to pass it through the CheckLetter function
-	*/
-	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-	console.log("this is the letter we typed", letterGuessed)
-	checkLetters(letterGuessed)
-	roundComplete();
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log("letter guessed by user", userGuess)
+	guessCheck(userGuess);
+	game();
 }
